a28167dd04f5df36a68ab7d69ccf904f461f4c9a
Sergey Batukov <sashachepuha@gmail.com>
2022-04-17 11:17:03 +0300

    theme update

--- clients/web/src/style/themes/dark.ts
+++ clients/web/src/style/themes/dark.ts
@@ -1,15 +1,46 @@
 import { ITheme } from "./theme.interface";
 
 const dark: ITheme = {
+  // BACKGROUNDS
+  // -- WRAPPERS
   bg_100: "#171822",
   bg_200: "#11121B",
   bg_300: "#171822",
+  // -- INPUT
+  input_bg: "#171822",
+  input_hover_bg: "#8785ff",
+  input_active_bg: "#615EFF",
+  // -- BUTTON
+  btn_bg: "#171822",
+  btn_hover_bg: "#8785ff",
+  btn_active_bg: "#615EFF",
+  // -- SCROLL
+  scroll: "#171822",
+  scroll_thumb: "#615EFF",
+
+  // TEXT
+  // -- FONTS
+  font_100: "Inter",
+  font_200: "IBM Plex Mono",
+  // -- COLORS
   text_100: "rgba(255,255,255,0.8)",
   text_200: "rgba(255,255,255,0.6)",
-  text_300: "rgba(255,255,255,0.5)",
-  accent_100: "#615EFF",
-  accent_200: "#615EFF",
-  accent_300: "#615EFF",
+  text_300: "rgba(255,255,255,0.4)",
+
+  // UX STATUSES
+  // -- OK
+  ok_100: "rgba(144,238,144, 1)",
+  ok_200: "rgba(144,238,144, 0.5)",
+  ok_300: "rgba(144,238,144, 0.2)",
+  // -- WARNING
+  warning_100: "rgba(255,228,60, 1)",
+  warning_200: "rgba(255,228,60, 0.5)",
+  warning_300: "rgba(255,228,60, 0.2)",
+  // -- CRITICAL
+  critical_100: "rgba(255,79,15, 1)",
+  critical_200: "rgba(255,79,15, 0.5)",
+  critical_300: "rgba(255,79,15, 0.2)",
 };
 
 export default dark;
+
--- clients/web/src/style/themes/theme.interface.ts
+++ clients/web/src/style/themes/theme.interface.ts
@@ -1,11 +1,42 @@
 export interface ITheme {
+  // BACKGROUNDS
+  // -- WRAPPERS
   bg_100: string;
   bg_200: string;
   bg_300: string;
+  // -- INPUT
+  input_bg: string;
+  input_hover_bg: string;
+  input_active_bg: string;
+  // -- BUTTON
+  btn_bg: string;
+  btn_hover_bg: string;
+  btn_active_bg: string;
+  // -- SCROLL
+  scroll: string;
+  scroll_thumb: string;
+
+  // TEXT
+  // -- FONTS
+  font_100: string;
+  font_200: string;
+  // -- COLORS
   text_100: string;
   text_200: string;
   text_300: string;
-  accent_100: string;
-  accent_200: string;
-  accent_300: string;
+
+  // UX STATUSES
+  // -- OK
+  ok_100: string;
+  ok_200: string;
+  ok_300: string;
+  // -- WARNING
+  warning_100: string;
+  warning_200: string;
+  warning_300: string;
+  // -- CRITICAL
+  critical_100: string;
+  critical_200: string;
+  critical_300: string;
 }
+
