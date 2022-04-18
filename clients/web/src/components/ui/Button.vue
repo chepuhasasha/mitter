55b2365fc5314481de69966666af88fcc89b5e7f
Sergey Batukov <sashachepuha@gmail.com>
2022-04-18 11:20:17 +0300

    update

--- clients/web/src/components/ui/Button.vue
+++ clients/web/src/components/ui/Button.vue
@@ -136,12 +136,16 @@ export default defineComponent({
   &_active {
     background: var(--btn_active_bg);
     color: var(--text_100);
+    box-shadow: 0 0 30px -8px var(--btn_active_bg);
+    border-color: var(--btn_active_bg);
 
     &:hover {
       background: var(--btn_hover_bg);
+      border-color: var(--btn_hover_bg);
     }
   }
   &_disable {
+    cursor: not-allowed;
     opacity: 0.5;
 
     &:hover {
@@ -150,3 +154,4 @@ export default defineComponent({
   }
 }
 </style>
+
