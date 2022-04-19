4072b1f122d5bfbafe999f08933bf013d4a2db56
Sergey Batukov <sashachepuha@gmail.com>
2022-04-19 11:33:17 +0300

    add mongoDB

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+    AuthModule,
+  ],
   controllers: [AuthController],
   providers: [],
 })
