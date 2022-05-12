f2d84610a0ba67a4683fa953a41c7d047c17e006
Sergey Batukov <sashachepuha@gmail.com>
2022-05-12 09:45:04 +0300

    add localization directive

---
+++ clients/web/src/plagins/localization.ts
@@ -0,0 +1,16 @@
+import { App } from "vue";
+
+export default {
+  install: (Vue: App): void => {
+    Vue.directive("lang", (el: HTMLElement, binding) => {
+      const content = el.innerText.split("$"); \
+      content.shift()
+      content.forEach(text => {
+        if (text.slice(0, 2) === binding.value) {
+          el.innerText = text.slice(2)
+        }
+      })
+    });
+  },
+};
+
