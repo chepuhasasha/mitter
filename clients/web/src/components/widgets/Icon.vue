28512f545b8c58dd0a58934b86a87000d2da6c5e
Sergey Batukov <sashachepuha@gmail.com>
2022-05-13 11:04:31 +0300

    add new icons

--- clients/web/src/components/widgets/Icon.vue
+++ clients/web/src/components/widgets/Icon.vue
@@ -1,6 +1,6 @@
 // COMPLETE: [Icon.vue]
 <template lang="pug">
-svg(viewBox='0 0 512 512' :width='size' :height='size' @click='click' :style='getStyle')
+svg(viewBox='0 0 14 14' :width='size' :height='size' @click='click' :style='getStyle')
   path(v-for='path in getPaths' :d='path' :fill='fill')
 </template>
 <script lang="ts" setup>
