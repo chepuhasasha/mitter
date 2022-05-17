103f2bfd99978b2e8169dcc4835e4fd6e9c1edd3
Sergey Batukov <sashachepuha@gmail.com>
2022-05-17 20:43:02 +0300

    add v-lang

--- clients/web/src/main.ts
+++ clients/web/src/main.ts
@@ -4,7 +4,7 @@ import router from "./router";
 import { key, store } from "./store";
 import "./style/main.less";
 import container from "./plagins/container";
-import localization from "./plagins/localization";
+import { VLang } from "@chepuhasasha/v-lang";
 import ui from "./components/ui";
 import widgets from "./components/widgets";
 
@@ -12,7 +12,7 @@ createApp(App)
   .use(store, key)
   .use(router)
   .use(container)
-  .use(localization)
+  .use(VLang)
   .use(widgets)
   .use(ui)
   .mount("#app");
