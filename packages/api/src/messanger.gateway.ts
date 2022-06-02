04acd0100acf9cb94f95984f266536561c6c3c89
Sergey Batukov <sashachepuha@gmail.com>
2022-06-02 09:37:47 +0300

    updage messanger gateway

--- packages/api/src/messanger.gateway.ts
+++ packages/api/src/messanger.gateway.ts
@@ -5,14 +5,29 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { IMessage, ICodeMessage, IUtilizationMessage } from '@mitter/types';
+import { UseGuards } from '@nestjs/common';
+import { MessangerGuard } from './guards/messanger.guard';
 
 @WebSocketGateway()
 export class MessangerGateway {
+  handleConnection(socket: Socket): WsResponse<string> {
+    const room = socket.handshake.auth.token.split(':')[0];
+    if (room) {
+      socket.join(room);
+      return { event: 'room_connected', data: room };
+    }
+    return { event: 'error', data: 'Invalid token' };
+  }
+
+  @UseGuards(MessangerGuard)
   @SubscribeMessage('message')
   onMessage(
     socket: Socket,
     data: IMessage | ICodeMessage | IUtilizationMessage,
   ): WsResponse<IMessage | ICodeMessage | IUtilizationMessage> {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit(`event_${data.type}`, data);
+    });
     return { event: `event_${data.type}`, data };
   }
 }
