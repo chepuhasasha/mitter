8bb832ee6e230d13957f53733d8b8c3656f5496f
Sergey Batukov <sashachepuha@gmail.com>
2022-06-02 09:30:23 +0300

    hot fix

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ChanelsModule } from './chanels/chanels.module';
 import { UserModule } from './user/user.module';
 import { RoomModule } from './room/room.module';
 import { MessangerGateway } from './messanger.gateway';
 @Module({
-  imports: [ChanelsModule, UserModule, RoomModule],
+  imports: [UserModule, RoomModule],
   providers: [MessangerGateway],
 })
 export class AppModule {}
